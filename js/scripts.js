e12838a91e9389c2c4bdfd468683e81a46d2a844
Tawnee Harris <tawneeh@icloud.com>
2020-10-30 16:45:11 -0700

    add includes method to function

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,41 @@
-const countUp = [0, "Beep!", "Boop!", "Won't you be my neighbor?"]
+const countUp = [0, "Beep!"]
+const count2 = [0, "Beep!", "Boop!"]
+const count3 = [0, "Beep!", "Boop!", "Won't you be my neighbor?"]
 
 function beepBoop(userInput) {
-  for (i = 0; i < countUp.length; i++) {
+  for (i = 0; i < count3.length; i++) {
   if (userInput === 0) {
     return 0;
   } else if (userInput === 1) {
     return countUp;
+  } else if (userInput === 2) {
+    return count2;
+  } else if (userInput === 3) {
+    return count3;
+  }
+
   }
 }
+
+function beepBoop2(numberString) {
+  if (numberString.includes("3")) {
+    return "Won't you be my neighbor?";
+  } else if (numberString.includes("2")) {
+    return "Boop!";
+  } else if (numberString.includes("1")) {
+    return "Beep!";
+}
 }
 
 
-document.getElementById("#output").submit();
+//document.getElementById("#output").submit();
 
 
 $(document).ready(function() {
   $("form#roboCount").submit(function(event) {
     event.preventDefault();
 
-
-
-$("#clearButton").click(function() {
-    $("#userNumber").hide();
-    userNumber = [];
-  }) 
-
+  $("#results").show();
 
   });
 });
