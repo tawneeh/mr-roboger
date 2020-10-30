04fea44908c5b7e014fa166484ced6693fae5612
Tawnee Harris <tawneeh@icloud.com>
2020-10-30 10:46:41 -0700

    add test one passing

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,35 @@
+function beepBoop() {
+  let userNumber  = parseInt($("#userNumber").val());
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 $(document).ready(function() {
   $("form#roboCount").submit(function(event) {
     event.preventDefault();
-
-    $("#output").text(result);
 
   });
 });
