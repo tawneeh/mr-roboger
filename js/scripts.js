8c1fc05dda4589d8ea11b60e0653dc9d33b86fe6
Tawnee Harris <tawneeh@icloud.com>
2020-10-30 14:41:18 -0700

    attempt to add a for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
-const countUp = [0,"Beep!"]
+const countUp = [0, "Beep!", "Boop!", "Won't you be my neighbor?"]
 
 function beepBoop(userInput) {
+  for (i = 0; i < countUp.length; i++) {
   if (userInput === 0) {
     return 0;
   } else if (userInput === 1) {
     return countUp;
   }
+}
 }
 
 
